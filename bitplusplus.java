33ca8e67d18cb6f1baf8cf94a9b7c11fb3c4663b
Paulie-Aditya <paul.aditya304@gmail.com>
2023-09-05 16:34:08 +0530

    Solved problem 282 A: Bit++ https://codeforces.com/problemset/problem/282/A

---
+++ bitplusplus.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class bitplusplus {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        int x = 0;
+
+
+        for(int i = 0;i<n;i++)
+        {
+            String s = sc.nextLine();
+            if (s.charAt(0) == '-' || s.charAt(1) == '-' || s.charAt(2) == '-') {
+                x--;
+            } else {
+                x++;
+            }
+        }
+        System.out.println(x);
+    }
+}
