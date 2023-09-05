172896b8d0bc6b5a635db4bc6b04ef38b9e08101
Paulie-Aditya <paul.aditya304@gmail.com>
2023-09-05 16:45:15 +0530

    Solved Problem 263 A: Beautiful Matrix https://codeforces.com/problemset/problem/263/A

---
+++ beautiful_matrix.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class beautiful_matrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int ans = 0;
+        // Middle is 3,3
+        for (int i = 1; i < 6; i++) {
+            for (int j = 1; j < 6; j++) {
+                if (sc.nextInt() == 1) {
+                    ans = Math.abs(3 - i) + Math.abs(3 - j);
+                }
+            }
+        }
+        System.out.println(ans);
+    }
+}
