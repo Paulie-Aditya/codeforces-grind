a4bbcc3530a08b64311e9ba18e7e18d40eb49155
Paulie-Aditya <paul.aditya304@gmail.com>
2023-09-12 13:02:34 +0530

    Solved Problem 339 A https://codeforces.com/problemset/problem/339/A

---
+++ helpful_maths.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class helpful_maths {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1') {
+                arr.add(1);
+            } else if (s.charAt(i) == '2') {
+                arr.add(2);
+            } else if (s.charAt(i) == '3') {
+                arr.add(3);
+            }
+
+        }
+        Collections.sort(arr);
+        StringBuilder to_return = new StringBuilder("");
+        for (int i = 0; i < arr.size(); i++) {
+            to_return.append(arr.get(i));
+            to_return.append('+');
+        }
+        to_return.deleteCharAt(s.length());
+
+        System.out.println(to_return.toString());
+
+    }
+}
