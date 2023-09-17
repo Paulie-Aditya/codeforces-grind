e99c555459d6137ac5af2e48da3b6d9829a31cbc
Paulie-Aditya <paul.aditya304@gmail.com>
2023-09-17 06:56:54 +0530

    Solved Problem: 546 A https://codeforces.com/problemset/problem/546/A

---
+++ soldier_banana.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class soldier_banana {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int k = sc.nextInt();
+        int n = sc.nextInt();
+        int w = sc.nextInt();
+
+        int b = 0;
+        int i = 1;
+
+        while(w>0){
+        
+            if(!(n - k*i > 0)){
+                b+= -1*(n- k*i);
+                n = 0;
+            }
+            else{
+                n-= i*k;
+            }
+            i++;
+            w--;
+        }
+
+        System.out.println(b);
+
+        
+    }
+}
