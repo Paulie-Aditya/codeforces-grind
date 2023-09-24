ea098240d54451ce462e40cedc2e6e9ccc380bed
Paulie-Aditya <paul.aditya304@gmail.com>
2023-09-24 20:54:37 +0530

    Solved Problem 41 A
    https://codeforces.com/problemset/problem/41/A

---
+++ translate.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class translate {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String t = sc.nextLine();
+
+        int n = s.length();
+        if (t.length() != n) {
+            System.out.println("NO");
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) != t.charAt(n - 1 - i)) {
+                System.out.println("NO");
+                return;
+            }
+        }
+        System.out.println("YES");
+
+    }
+}
